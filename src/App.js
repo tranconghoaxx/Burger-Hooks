59d0063e17e426b8d36590ce348506dde3860947
tranconghoaxx <tranhoa300696@gmail.com>
2020-04-25 00:44:47 +0700

    setting the rith useEffect

--- src/App.js
+++ src/App.js
@@ -21,9 +21,10 @@ const Auth = React.lazy(() => {
 });
 
 const App = props => {
+  const {onTryAutoSignup} = props;
   useEffect(() => {
-    props.onTryAutoSignup();
-  },[]);
+    onTryAutoSignup();
+  },[onTryAutoSignup]);
     let routes = (
       <Switch>
           <Route path="/auth" render={(props) => <Auth {...props}/>} />
--- src/containers/BurderBuilder/BurderBuilder.js
+++ src/containers/BurderBuilder/BurderBuilder.js
@@ -20,9 +20,11 @@ const INGREDIENT_PRICES = {
 };
 const  BurgerBuilder = props => {
     const [purchasing,setPurchasing] = useState(false);
+
+    const {onInitIngredients} = props;
     useEffect(() => {
-        props.onInitIngredients();
-    },[]);
+        onInitIngredients();
+    },[onInitIngredients]);
 
     const updatePurchaseState = (ingredients) =>  {
         const sum = Object.keys(ingredients)
